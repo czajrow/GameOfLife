094d7b76e2aa432b14a46c15fc3dbe29af07e4e6
czajkaa1 <czajka.adam147@gmail.com>
2019-02-08 13:23:44 +0100

    ostateczna wersja projektu

---
+++ life/src/easteregg.h
@@ -0,0 +1,6 @@
+#ifndef _GENERACJA_H
+#define _GENERACJA_H
+
+void easteregg(void);
+
+#endif
---
+++ life/src/generacja.h
@@ -0,0 +1,8 @@
+#ifndef _GENERACJA_H
+#define _GENERACJA_H
+
+void generuj8(void);
+
+void generuj4(void);
+
+#endif
